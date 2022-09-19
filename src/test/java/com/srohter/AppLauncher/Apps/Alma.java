69c83394de60b635676274f656e278f294f7936d
skylarrohter <srohter@waprep.org>
2022-09-19 09:40:50 -0700

    Added mac support to Alma class.

--- src/test/java/com/srohter/AppLauncher/Apps/Alma.java
+++ src/test/java/com/srohter/AppLauncher/Apps/Alma.java
@@ -1,5 +1,7 @@
 package com.srohter.AppLauncher.Apps;
 
+import org.junit.jupiter.api.DisplayName;
+
 import java.awt.Robot;
 import java.awt.event.InputEvent;
 
@@ -8,12 +10,22 @@ public class Alma {
         try {
             Runtime runtime = Runtime.getRuntime();
             Robot r = new Robot();
-            String[] s = {"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe", "https://waprep.getalma.com"};
-            runtime.exec(s);
-            Thread.sleep(2000);
-            r.mouseMove(-964,480);
-            r.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-            r.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+            String[] win = {"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe", "https://waprep.getalma.com"};
+            String[] mac = {"/usr/bin/open", "-a", "/Applications/Google Chrome.app", "https://waprep.getalma.com"};
+            String os = System.getProperty("os.name");
+            if(os.contains("Mac")){
+                runtime.exec(mac);
+                Thread.sleep(3000);
+                r.mouseMove(834,573);
+                r.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+                r.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+            }else if(os.contains("Win")){
+                runtime.exec(win);
+                Thread.sleep(2000);
+                r.mouseMove(-964,480);
+                r.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+                r.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
